504b3f41bab80433e9ab135ba92d41a74ba43c5b
Lucas Fernandez <lfvillafranca@gmail.com>
2018-06-28 21:24:34 -0300

    Heightmap

---
+++ Assets/TP2_Heightmap/HeightMap.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeightMap : MonoBehaviour
+{
+    public Material Material;
+
+    [Header("Biome Transitions")]
+    [Range(0, 1)]
+    public float BiomeTransition;
+    [Range(0, 1)]
+    public float HighTransition;
+    [Range(0, 1)]
+    public float MediumTransition;
+    [Range(0, 1)]
+    public float LowTransition;
+    
+    [Header("Heighmap Params")]
+    [Range(1, 5)]
+    public float Tesselation;
+    [Range(0, 5)]
+    public float Offset;
+
+    public void OnBiomeChange(Slider slider)
+    {
+        BiomeTransition = slider.value;
+        HighTransition = slider.value;
+        MediumTransition = slider.value;
+        LowTransition = slider.value;
+        
+        UpdateMaterial();
+    }
+    
+    public void OnTesselationChange(Slider slider)
+    {
+        Tesselation = slider.value;
+        
+        UpdateMaterial();
+    }
+    
+    public void OnOffsetChange(Slider slider)
+    {
+        Offset = slider.value;
+        
+        UpdateMaterial();
+    }
+
+    public void UpdateMaterial()
+    {
+        Material.SetFloat("_HighLerp", HighTransition);
+        Material.SetFloat("_MediumLerp", MediumTransition);
+        Material.SetFloat("_LowLerp", LowTransition);
+        Material.SetFloat("_Tesselation", Tesselation);
+        Material.SetFloat("_Offset", Offset);
+    }
+}
