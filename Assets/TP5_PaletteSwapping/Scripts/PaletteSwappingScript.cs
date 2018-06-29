7d751e301202ee8325369536499db2c6950c4adc
Lucas Fernandez <lfvillafranca@gmail.com>
2018-06-28 21:45:15 -0300

    Fixed Palette swapping

--- Assets/TP5_PaletteSwapping/Scripts/PaletteSwappingScript.cs
+++ Assets/TP5_PaletteSwapping/Scripts/PaletteSwappingScript.cs
@@ -1,23 +1,35 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
+[ExecuteInEditMode]
 public class PaletteSwappingScript : MonoBehaviour
 {
-    public Material Material;
-
+    [Header("Ramps")]
+    [Range(0, 3)]
+    public int CurrentRamp;
     public Texture2D[] Ramps;
-
+    public Material PostProcessMaterial;
+    
+    [Header("Textures")]
     [Range(0, 3)]
     public int CurrentTexture;
+    public Texture2D[] Textures;
+    public Material TextureMaterial;
 
+    public void ChangeCurrentRamp(Slider slider)
+    {
+        CurrentRamp = (int)slider.value;
+    }
+    
     public void ChangeCurrentTexture(Slider slider)
     {
         CurrentTexture = (int)slider.value;
+        TextureMaterial.SetTexture("_MainTex", Textures[CurrentTexture]);
     }
 
     private void OnRenderImage(RenderTexture src, RenderTexture dest)
     {
-        Material.SetTexture("_Ramp", Ramps[CurrentTexture]);
-        Graphics.Blit(src, dest, Material);
+        PostProcessMaterial.SetTexture("_Ramp", Ramps[CurrentRamp]);
+        Graphics.Blit(src, dest, PostProcessMaterial);
     }
 }
